03b0c78d1d9608c89f09c9a3e4f7a476f95189ec
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-05-03 10:35:44 +0200

    Mejorar imagen, scroll con singlechildscrollview

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -7,62 +7,71 @@ class BasicoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: <Widget>[
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+
           _crearImagen(),
           _crearTitulo(),
           _crearAcciones(),
           _crearTexto(),
           _crearTexto(),
           _crearTexto()
-        ],
+
+          ],
+        ),
       )
     );
   }
 
   Widget _crearImagen() {
     return Image(
+      width: double.infinity,
       image: NetworkImage(
           'https://images.pexels.com/photos/132037/pexels-photo-132037.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+      fit: BoxFit.cover,
+      height: 200.0,
     );
   }
 
   Widget _crearTitulo() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-      child: Row(
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
 
-        children: <Widget>[
-          Expanded(
+          children: <Widget>[
+            Expanded(
 
-            child: Column(
+              child: Column(
 
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'Lago con un embarcadero',
-                  style: estiloTitulo,
-                ),
-                SizedBox(
-                  height: 7.0,
-                ),
-                Text(
-                  'Un lago que en EEUU',
-                  style: estiloSubtitulo,
-                )
-              ],
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Lago con un embarcadero',
+                    style: estiloTitulo,
+                  ),
+                  SizedBox(
+                    height: 7.0,
+                  ),
+                  Text(
+                    'Un lago que en EEUU',
+                    style: estiloSubtitulo,
+                  )
+                ],
+              ),
             ),
-          ),
-          Icon(
-            Icons.star,
-            color: Colors.red,
-            size: 30.0,
-          ),
-          Text(
-            '41',
-            style: TextStyle(fontSize: 20.0),
-          ),
-        ],
+            Icon(
+              Icons.star,
+              color: Colors.red,
+              size: 30.0,
+            ),
+            Text(
+              '41',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -95,11 +104,13 @@ class BasicoPage extends StatelessWidget {
 
   Widget _crearTexto() {
 
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-      child: Text(
-        'Id voluptate enim pariatur cillum pariatur ut consequat proident in proident aliqua Lorem laborum. Excepteur reprehenderit nostrud ad aliquip consectetur anim quis occaecat tempor voluptate veniam. Elit aliquip aliquip sint culpa ipsum enim consequat voluptate reprehenderit et voluptate in tempor labore. Adipisicing nostrud aliqua laboris reprehenderit reprehenderit proident. Duis deserunt tempor Lorem nostrud officia ad proident consectetur pariatur ullamco non.',
-        textAlign: TextAlign.justify,
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40.0,),
+        child: Text(
+          'Id voluptate enim pariatur cillum pariatur ut consequat proident in proident aliqua Lorem laborum. Excepteur reprehenderit nostrud ad aliquip consectetur anim quis occaecat tempor voluptate veniam. Elit aliquip aliquip sint culpa ipsum enim consequat voluptate reprehenderit et voluptate in tempor labore. Adipisicing nostrud aliqua laboris reprehenderit reprehenderit proident. Duis deserunt tempor Lorem nostrud officia ad proident consectetur pariatur ullamco non.',
+          textAlign: TextAlign.justify,
+        ),
       ),
     );
   }
