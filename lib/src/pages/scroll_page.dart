7a0992a51bd356281b7a818dfd20685b2735ad71
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-05-03 11:07:53 +0200

    Dos paginas de scroll con fondo y expanded

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -14,15 +14,77 @@ class ScrollPage extends StatelessWidget {
     );
   }
 
+/*
+En stack el orden importa, los primeros son los que están más abajo
+y los últimos los que están más arriba
+*/
   Widget _pagina1() {
-    return Center(
-      child: Text('Pagina 1'),
+    return Stack(
+      children: <Widget>[
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
     );
   }
 
+  Widget _colorFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _imagenFondo() {
+
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/original.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+
+  }
+
+  Widget _textos() {
+
+    final estilotexto = TextStyle(color: Colors.white, fontSize: 50.0);
+
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 20.0,),
+          Text('11º', style: estilotexto,),
+          Text('Miércoles', style: estilotexto,),
+          Expanded( child: Container(),),
+          Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white,)
+        ],
+      ),
+    );
+  }
+
+
+
   Widget _pagina2() {
-    return Center(
-      child: Text('Pagina 2'),
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+      child: Center(
+        child: RaisedButton(
+          shape: StadiumBorder(),
+          color: Colors.blue,
+          textColor: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+            child: Text('Bienvenidos', style: TextStyle(fontSize: 20.0),),
+          ),
+          onPressed: (){},
+        ),
+      ),
     );
   }
 }
